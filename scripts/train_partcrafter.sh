f33f33e9dc89140a50eeaa8f7853b94dd842691f
QuantaJi <quantaji@github.com>
2025-11-04 20:21:18 -0800

    dd

--- scripts/train_partcrafter.sh
+++ scripts/train_partcrafter.sh
@@ -1,11 +1,15 @@
 NUM_MACHINES=1
-NUM_LOCAL_GPUS=8
+NUM_LOCAL_GPUS=1
 MACHINE_RANK=0
 
 WOKRDIR="$(realpath -m -- "$(pwd)/$(dirname "$0")/..")"
 export PYTHONPATH=${WOKRDIR}:${PYTHONPATH}
 export WANDB_API_KEY="" # Modify this if you use wandb
 export WANDB_MODE=offline
+export PYOPENGL_PLATFORM=osmesa 
+export LIBGL_ALWAYS_SOFTWARE=1 
+export MESA_LOADER_DRIVER_OVERRIDE=swrast 
+export OSMESA_LIBRARY=/usr/lib/x86_64-linux-gnu/libOSMesa.so.8 
 
 accelerate launch \
     --num_machines $NUM_MACHINES \
--- scripts_guangda/04_2_test_command.sh
+++ scripts_guangda/04_2_test_command.sh
@@ -10,7 +10,23 @@ bash scripts/train_partcrafter.sh --config scripts_guangda/04_0_train_local_test
 # test for gradient accumulation step
 # not chainging usage
 
+
+# salloc --time=1:30:0 --ntasks=1 --gres=gpu:nvidia_h100_80gb_hbm3_3g.40gb:1 --mem=64G --nodes=1
+
 # apptainer nest
+conda_home="$(conda info | grep "active env location : " | cut -d ":" -f2-)"
+conda_home="${conda_home#"${conda_home%%[![:space:]]*}"}"
+
+export CUDA_HOST_COMPILER="$conda_home/bin/gcc"
+export CUDA_PATH="$conda_home"
+export CUDA_HOME=$CUDA_PATH
+export PATH="$CUDA_HOME/bin:$PATH"
+export LD_LIBRARY_PATH="$CUDA_HOME/lib64:$LD_LIBRARY_PATH"
+export PYOPENGL_PLATFORM=osmesa 
+export LIBGL_ALWAYS_SOFTWARE=1 
+export MESA_LOADER_DRIVER_OVERRIDE=swrast 
+export OSMESA_LIBRARY=/usr/lib/x86_64-linux-gnu/libOSMesa.so.8 
+
 bash scripts/train_partcrafter.sh --config scripts_guangda/04_1_train_cc_test.yaml --use_ema \
   --gradient_accumulation_steps 4 \
   --output_dir output_partcrafter \
